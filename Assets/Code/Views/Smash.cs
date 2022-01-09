b8bba2765f9d5e82cdea2158ca2174b8075904b8
Carla Florencia Sosa Padilla <carla_sp88@hotmail.com>
2022-01-09 17:39:35 -0300

    Adds smash from left

--- Assets/Code/Views/Smash.cs
+++ Assets/Code/Views/Smash.cs
@@ -7,40 +7,76 @@ using UnityEngine;
 namespace Code.Views
 {
     [RequireComponent(typeof(Collider2D))]
+    [RequireComponent(typeof(Rigidbody2D))]
     public class Smash : BaseHazardView
     {
         private Collider2D _collider;
+        private Rigidbody2D _rigidBody;
         private PlayerPresenter _playerPresenter;
 
         // Use this for initialization
         void Start()
         {
             _collider = GetComponent<Collider2D>();
+            _rigidBody = GetComponent<Rigidbody2D>();
         }
 
         // Update is called once per frame
-        void FixedUpdate()
+        void Update()
         {
-            CheckHit(transform.position);
+            CheckHits(transform.position);
         }
 
-        private void CheckHit(Vector3 position)
+        private void CheckHits(Vector3 position)
         {
-            List<RaycastHit2D> hits = CreateRays(position);
-            hits.ForEach(hit => Debug.DrawRay(hit.centroid, Vector3.down));
-            var isSteppedOnSomething = hits.Any(hit => hit.collider && hit.distance < _collider.bounds.size.y*0.5f);
-            if (isSteppedOnSomething)
+            HitSomethingVertically(position);
+            HitSomethingRight(position);
+        }
+
+        private void HitSomethingVertically(Vector3 position)
+        {
+            List<RaycastHit2D> verticalHits = CreateVerticalRays(position);
+            verticalHits.ForEach(hit => Debug.DrawRay(hit.centroid, Vector3.down));
+            var hitSomethingVertically = verticalHits.Any(hit => hit.collider && hit.distance < _collider.bounds.size.y * 0.5f);
+            if (hitSomethingVertically)
             {
+                Debug.Log("Player Hit from top.");
+                enabled = false;
+                _playerPresenter.DieSmashed();
+            }
+        }
+        private void HitSomethingRight(Vector3 position)
+        {
+            List<RaycastHit2D> hits = CreateRightHorizontalRays(position);
+            hits.ForEach(hit => Debug.DrawLine(hit.centroid, hit.centroid+ Vector2.right, Color.red,1.0f));
+            var hitSomething = hits.Any(hit => hit.collider && hit.distance < _collider.bounds.size.x * 0.5f);
+            if (hitSomething && _rigidBody.velocity.x > 0)
+            {
+                Debug.Log("Player Hit from the back.");
                 enabled = false;
                 _playerPresenter.DieSmashed();
             }
         }
 
-        private List<RaycastHit2D> CreateRays(Vector3 position) => new List<RaycastHit2D>()
+        private List<RaycastHit2D> CreateVerticalRays(Vector3 position) => new List<RaycastHit2D>()
         {
             Physics2D.Raycast(new Vector2(position.x - _collider.bounds.size.x*0.33f, position.y), Vector2.down, 1, 1 << 8),
             Physics2D.Raycast(new Vector2(position.x, position.y), Vector2.down, 1, 1 << 8),
             Physics2D.Raycast(new Vector2(position.x + _collider.bounds.size.x*0.33f, position.y), Vector2.down, 1, 1 << 8)
+        };
+
+        private List<RaycastHit2D> CreateRightHorizontalRays(Vector3 position) => new List<RaycastHit2D>()
+        {
+            Physics2D.Raycast(new Vector2(position.x , position.y- _collider.bounds.size.y*0.33f), Vector2.right, 1, 1 << 8),
+            Physics2D.Raycast(new Vector2(position.x, position.y), Vector2.right, 1, 1 << 8),
+            Physics2D.Raycast(new Vector2(position.x , position.y+ _collider.bounds.size.y*0.33f), Vector2.right, 1, 1 << 8)
+        };
+
+        private List<RaycastHit2D> CreateLeftHorizontalRays(Vector3 position) => new List<RaycastHit2D>()
+        {
+            Physics2D.Raycast(new Vector2(position.x , position.y- _collider.bounds.size.y*0.33f), Vector2.left, 1, 1 << 8),
+            Physics2D.Raycast(new Vector2(position.x, position.y), Vector2.left, 1, 1 << 8),
+            Physics2D.Raycast(new Vector2(position.x , position.y+ _collider.bounds.size.y*0.33f), Vector2.left, 1, 1 << 8)
         };
 
         public override void Attach(PlayerPresenter playerPresenter)
